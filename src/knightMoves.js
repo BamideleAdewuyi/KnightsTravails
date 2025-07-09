acc9a02581d6f5b355c5257f33e5896526451fe3
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-09 22:56:30 +0100

    knightMoves.js: Started writing calculate method

--- src/knightMoves.js
+++ src/knightMoves.js
@@ -2,9 +2,12 @@ class Traverse {
     constructor(start, finish) {
         this.start = start;
         this.finish = finish;
-    }
+    };
 
-    calculate() {
+    calculate(node = this.start) {
+        let current = node;
+        let q = [node];
+        let vis = [node];
 
     };
 };
