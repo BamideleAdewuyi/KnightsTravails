71c32e707311b60026f29b9717c7406606e710a0
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-10 00:59:58 +0100

    knightMoves.js: Wrote some of possibleMoves method and created removeItem method

--- src/knightMoves.js
+++ src/knightMoves.js
@@ -4,15 +4,24 @@ class Traverse {
         this.finish = finish;
     };
 
-    possibleMoves(node) {
+    removeItem(arr, value) {
 
+    };
+    
+    possibleMoves(node) {
+        let possibilities = [[node[0]-1, node[1]+2], [node[0]+1, node[1]+2],
+                            [node[0]+2, node[1]+1], [node[0]+2, node[1]-1],
+                            [node[0]+1, node[1]-2], [node[0]-1, node[1]-2],
+                            [node[0]-2, node[1]-1], [node[0]-2, node[1]+1]];
+        
+        return possibilities;
     };
 
     calculate(node = this.start) {
         let current = node;
         let q = [node];
         let vis = [node];
-
+        console.log(this.possibleMoves(node))
     };
 };
 
