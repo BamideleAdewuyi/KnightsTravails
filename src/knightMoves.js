95e7223d7b6861cda5a0e4ec66330a8a7508d36a
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-11 00:26:25 +0100

    knightMoves.js: Imported checkIncludes function and started writing knightMoves function

--- src/knightMoves.js
+++ src/knightMoves.js
@@ -1,5 +1,15 @@
-import { possibleMoves, checkMatch } from "./utility.js";
+import { possibleMoves, checkMatch, checkIncludes } from "./utility.js";
 
 export function knightMoves(start, finish) {
+    let q = [[start]];
+    let vis = [start];
 
+    while (q.length > 0) {
+        const current = q.shift();
+        if (checkMatch(current, finish)) {
+            return current;
+        }
+        const moves = possibleMoves(current);
+
+    }
 }
