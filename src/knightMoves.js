18ded0b75578dfe68a1d08a42c11cc3b83445745
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-11 01:20:49 +0100

    knightMoves.js: Wrote knightMoves function. Need to change possibleMoves function so it does not allow positions more with numbers above 7, as this would be out of chessboard bounds

--- src/knightMoves.js
+++ src/knightMoves.js
@@ -6,10 +6,17 @@ export function knightMoves(start, finish) {
 
     while (q.length > 0) {
         const current = q.shift();
-        if (checkMatch(current, finish)) {
+        if (checkMatch(current[current.length-1], finish)) {
             return current;
         }
-        const moves = possibleMoves(current);
-
+        const moves = possibleMoves(current[current.length-1]);
+        for (const move of moves) {
+            if (!checkIncludes(move, vis)) {
+                let path = current.slice()
+                path.push(move)
+                q.push(path);
+                vis.push(move);
+            }
+        }
     }
 }
